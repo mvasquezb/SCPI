bd41e57264151bfadae842a05c62225260df2a61
Martín Vásquez <pedromvasquezb@gmail.com>
2019-03-11 00:46:13 -0500

    Add demo todo list app

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,26 @@
 package com.pmvb.scpiback
 
+import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder.*
+import io.javalin.staticfiles.Location
+
+data class Todo(val id: Long = -1, val title: String = "", val completed: Boolean = false)
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    var todos = arrayOf(Todo(123123123, "My very first todo", false))
+
+    val app = Javalin.create().apply {
+        port(7000)
+        enableStaticFiles("src/web", Location.EXTERNAL)
+    }.start()
+
+    app.routes {
+        get("/todos") { ctx ->
+            ctx.json(todos)
+        }
+        put("/todos") { ctx ->
+            todos = ctx.bodyAsClass(Array<Todo>::class.java)
+            ctx.status(204)
+        }
+    }
 }
