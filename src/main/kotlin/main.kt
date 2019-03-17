1d28b12fa6efadf2c45fdabb20dc05eb1ac19d24
Martín Vásquez <pedromvasquezb@gmail.com>
2019-03-17 18:08:02 -0500

    Separate setup

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,14 +6,20 @@ import io.javalin.staticfiles.Location
 
 data class Todo(val id: Long = -1, val title: String = "", val completed: Boolean = false)
 
-fun main(args: Array<String>) {
-    var todos = arrayOf(Todo(123123123, "My very first todo", false))
+lateinit var todos: Array<Todo>
 
+fun main(args: Array<String>) {
+    todos = arrayOf(Todo(123123123, "My very first todo", false))
     val app = Javalin.create().apply {
+        enableCorsForAllOrigins()
         port(7000)
-        enableStaticFiles("src/web", Location.EXTERNAL)
+        enableStaticFiles("src/web/public", Location.EXTERNAL)
     }.start()
 
+    routeSetup(app)
+}
+
+fun routeSetup(app: Javalin) {
     app.routes {
         get("/todos") { ctx ->
             ctx.json(todos)
