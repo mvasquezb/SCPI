0f3c9c90fa505f24b3bfd36245940f0513dec8e4
Martín Vásquez <pedromvasquezb@gmail.com>
2019-04-13 22:00:58 -0500

    Add configuration

--- src/main/kotlin/com/pmvb/scpiback/data/connection.kt
+++ src/main/kotlin/com/pmvb/scpiback/data/connection.kt
@@ -1,34 +1,46 @@
 package com.pmvb.scpiback.data
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource
 import com.mysql.cj.jdbc.MysqlDataSource
 import com.pmvb.scpiback.AppConfig
 import com.pmvb.scpiback.data.models.Models
 import io.requery.Persistable
-import io.requery.reactivex.KotlinReactiveEntityStore
 import io.requery.sql.KotlinConfiguration
 import io.requery.sql.KotlinEntityDataStore
 import io.requery.sql.SchemaModifier
 import io.requery.sql.TableCreationMode
+import javax.sql.CommonDataSource
 
-val connectionUrl = "jdbc:${AppConfig["DB_DRIVER"]}://${AppConfig["DB_HOST"]}:${AppConfig["DB_PORT"]}/" +
-        "${AppConfig["DB_DATABASE"]}"
+val connectionUrl = "jdbc:${AppConfig["DB_DRIVER"]}://${AppConfig["DB_HOST"]}:${AppConfig["DB_PORT"]}"
 
-val source = when(AppConfig["DB_DRIVER"].toString().toLowerCase()) {
-    "mysql" -> MysqlDataSource()
-    else -> MysqlDataSource() // Default is MySQL as well, for now
+val dataSource: CommonDataSource = when(AppConfig["DB_DRIVER"].toString().toLowerCase()) {
+    "mysql" -> MysqlDataSource().apply {
+        setURL("$connectionUrl/${AppConfig["DB_DATABASE"]}?serverTimezone=UTC")
+        user = AppConfig["DB_USERNAME"].toString()
+        password = AppConfig["DB_PASSWORD"].toString()
+    }
+    "sqlserver" -> SQLServerDataSource().apply {
+        url = "$connectionUrl;database=${AppConfig["DB_DATABASE"]}"
+        user = AppConfig["DB_USERNAME"].toString()
+        setPassword(AppConfig["DB_PASSWORD"].toString())
+    }
+    else -> throw IllegalArgumentException("Database driver not supported")
 }
 
-val dataSource = source.apply {
-    setUrl(connectionUrl)
-    user = AppConfig["DB_USERNAME"].toString()
-    password = AppConfig["DB_PASSWORD"].toString()
-}
 
 val configuration = KotlinConfiguration(
         dataSource = dataSource,
-        model = Models.DEFAULT
+        model = Models.DEFAULT,
+        useDefaultLogging = true
 ).apply {
     SchemaModifier(this).createTables(TableCreationMode.CREATE_NOT_EXISTS)
 }
 
 val dataStore = KotlinEntityDataStore<Persistable>(configuration)
+
+fun main(args: Array<String>) {
+    println(connectionUrl)
+    println(AppConfig["DB_USERNAME"])
+    println(AppConfig["DB_PASSWORD"])
+    println(dataSource::class.java)
+}
